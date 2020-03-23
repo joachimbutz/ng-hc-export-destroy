d3d5cb81d10c6498002eb123d929f55fa5ca347d
Joachim Butz <Joachim.Butz@wienit.at>
2020-03-23 19:30:49 +0100

    chart.destroy()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Chart} from 'angular-highcharts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-hc-export-destroy';
+export class AppComponent implements OnInit {
+  constructor() { }
+
+  chartExists = false;
+  chart: Chart;
+
+  private chartOptions: Highcharts.Options = {
+    chart: {
+      type: 'line'
+    },
+    title: {
+      text: 'Linechart'
+    },
+    credits: {
+      enabled: false
+    },
+    exporting: {
+      enabled: true
+    },
+    series: [{
+      type: 'line',
+      name: 'Line 1',
+      data: [1, 2, 3]
+    }]
+  };
+
+  ngOnInit(): void {
+    this.createChart();
+  }
+
+  destroyOrCreate() {
+    if (this.chartExists) {
+      this.destroyChart();
+    } else {
+      this.createChart();
+    }
+  }
+
+  private destroyChart() {
+    this.chart.destroy();
+    this.chartExists = false;
+  }
+
+  private createChart() {
+    this.chart = new Chart(this.chartOptions);
+    this.chartExists = true;
+  }
+
+  destroyOrCreateLabel() {
+    if (this.chartExists) {
+      return 'Destroy';
+    }
+    return 'Create';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {ChartModule, HIGHCHARTS_MODULES} from 'angular-highcharts';
+import * as exporting from 'highcharts/modules/exporting.src';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +10,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ChartModule
   ],
-  providers: [],
+  providers: [
+    { provide: HIGHCHARTS_MODULES, useFactory: () => [exporting]}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
